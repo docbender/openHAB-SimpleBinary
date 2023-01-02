da762647bfb855ee1588e34d9ba9caf64a3a05ce
docbender <vita.tucek@seznam.cz>
2023-01-02 09:05:00 +0100

    3.4.0

--- org.openhab.binding.simplebinary/src/main/java/org/openhab/binding/simplebinary/internal/SimpleBinaryBindingConstants.java
+++ org.openhab.binding.simplebinary/src/main/java/org/openhab/binding/simplebinary/internal/SimpleBinaryBindingConstants.java
@@ -24,7 +24,7 @@ import org.openhab.core.thing.type.ChannelTypeUID;
  */
 @NonNullByDefault
 public class SimpleBinaryBindingConstants {
-    public static final String VERSION = "3.3.0";
+    public static final String VERSION = "3.4.0";
 
     private static final String BINDING_ID = "simplebinary";
 
